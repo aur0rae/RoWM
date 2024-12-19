4f9951826790c1cbf27c2c0b8605f31d91eda7ad
aurorae-nb <nbadovick@gmail.com>
2024-12-19 00:56:06 -0500

    added wallpaper and install script

---
+++ install.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+# Determine distrobution and install the correct dependancies
+echo "Determining distribution..."
+
+deb_install() {
+	sudo apt update
+	sudo apt install -y nala feh kitty rofi picom thunar lxpolkit x11-xserver-utils pavucontrol build-essential libx11-dev libxft-dev libxinerama-dev libx11-xcb-dev libxcb-res0-dev xdg-utils git firefox-esr flatpak
+        sudo nala fetch
+}
+
+arch_install() {
+	sudo pacman -Syu --noconfirm
+	sudo pacman -S --noconfirm base-devel libx11 libxcb cmake libxft libxinerama libxcb-res xorg-xev xorg-xbacklight alsa-utils feh kitty rofi picom thunar lxpolkit pavucontrol git firefox flatpak
+	git clone https://aur.archlinux.org/paru.git && cd paru
+        makepkg -si
+	cd .. && rm -rf paru
+}
+
+opensuse_install() {
+	sudo zypper refresh
+	sudo zypper dist-upgrade
+	sudo zypper install -t devel_basis
+	sudo zypper install -y libX11-devel libxcb-devel libXft-devel libXinerama-devel xorg-x11-server thunar feh kitty rofi picom lxpolkit pavucontrol git firefox flatpak
+
+if [ -f "/etc/os-release" ]; then
+	source /etc/os-release
+	ID=$ID
+	case "$ID" in
+		debian|ubuntu)
+			echo "Using apt to install dependancies, base apps, Flatpak, and Nala..."
+			deb_install
+			;;
+		arch)
+			echo "Using pacman to install dependancies, base apps, Flatpak, and Paru..."
+			arch_install
+			;;
+		opensuse-leap|opensuse-tumbleweed)
+			echo "Using zypper to install dependancies, base apps, and Flatpak..."
+			opensuse_install
+			;;
+		*)
+			echo "Error: Unsupported. Exiting..."
+			exit
+			;;
+	esac
+else
+	echo "Error: /etc/os-release not found. Exiting..."
+	exit
+fi
+
+
+# Configure Flatpak
+echo "Configuring Flatpak..."
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+
+# Move dwm, dmenu, and slstatus into appropriate hidden folders under user's home directory and compile
+echo "Moving things around and building..."
+
+TEMP_DIR=$(pwd)
+
+for word in "dwm dmenu slstatus"; do
+	mv "$(TEMP_DIR)"/${word} ~/.${word} && cd ~/.${word} && sudo make clean install; 
+done
+
+
+# Editing bash and Xorg things
+echo "Configuring DWM to launch at login..."
+if [ !-f "~/.xinitrc" ]; then
+	touch ~/.xinitrc
+	echo -e ". .fehbg &\nslstatus &\nexec dwm" >> ~/.xinitrc
+else
+	echo "Error: ~/.xinitrc already exists. Exiting..."
+	exit
+fi
+
+if [ !-f "~/.bash_profile" ]; then
+	touch .bash_profile
+fi
+echo -e "if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then\n  startx\nfi" >> ~/.bash_profile
+
+
+# Configuring bash prompt
+echo "Setting custom bash prompt..."
+
+git clone https://github.com/aurorae-nb/mybash
+mv mybash/bashrc-simple-cyan ~/.bashrc
+rm -rf mybash
+
+
+# Theming
+echo "Installing themes (set with lxappearance)..."
+
+cd /usr/share/theme && sudo git clone https://github.com/EliverLara/Nordic
+cd "$(TEMP_DIR)"
+git clone https://github.com/alvatip/Nordzy-cursors && cd Nordzy-cursors
+./install.sh
+cd .. && rm -rf Nordzy-cursors
+mkdir -P ~/.config/kitty
+mv kitty.conf ~/.config/kitty/kitty.conf
+mv bg.png ~/Pictures/bg.png && feh --bg-fill ~/Pictures/bg.png
+
+# Notify user that process terminated successfully
+echo "Installation completed successfully. No errors reported."
