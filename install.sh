db3b6831f4b74a3038cec860d4b75ca854f3341f
Aurora <108196668+aur0rae@users.noreply.github.com>
2025-01-12 02:17:59 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ deb_install() {
 	sudo apt update
 	sudo apt upgrade
 	sudo apt install -y xorg x11-xserver-utils build-essential libx11-dev libxft-dev libxinerama-dev libx11-xcb-dev libxcb-res0-dev libimlib2-dev wget git
-	sudo apt install -y fonts-font-awesome feh policykit-1-gnome nala alacritty flameshot thunar lxappearance pavucontrol neovim flatpak
+	sudo apt install -y fonts-font-awesome feh policykit-1-gnome nala alacritty flameshot thunar lxappearance pavucontrol neovim flatpak lightdm lightdm-gtk-greeter
 
 	# Configure nala to use best available mirrors
 	sudo nala fetch
@@ -20,7 +20,7 @@ deb_install() {
 arch_install() {
 	sudo pacman -Syu --noconfirm
 	sudo pacman -S --noconfirm base-devel xorg-server xorg-xinit libx11 libxcb cmake libxft libxinerama wget git
-	sudo pacman -S --noconfirm awesome-terminal-fonts feh alacritty picom flameshot thunar polkit-gnome lxappearance pavucontrol neovim flatpak
+	sudo pacman -S --noconfirm awesome-terminal-fonts feh alacritty picom flameshot thunar polkit-gnome lxappearance pavucontrol neovim flatpak lightdm lightdm-gtk-greeter
 
 	# Install paru
 	git clone https://aur.archlinux.org/paru.git
@@ -84,15 +84,6 @@ cd $TEMP_DIR
 # Edit files to make DWM launch on startup and set background properly
 echo "Configuring DWM to launch on login..."
 
-if [[ ! -f "~/.xinitrc" ]]; then
-	echo "exec dwm" >> ~/.xinitrc
-else
-	echo "Error: ~/.xinitrc already exists. Exiting..."
-	exit
-fi
-
-echo -e 'if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then\n\tstartx\nfi' >> ~/.bash_profile
-
 # Configure bash prompt
 echo "Setting custom bash prompt..."
 rm $HOME/.bashrc
@@ -114,6 +105,12 @@ sudo git clone https://github.com/EliverLara/Nordic
 cd $TEMP_DIR
 mv src/gtk-3.0 $HOME/.config/
 
+# Configure LightDM
+sudo systemctl enable lightdm
+sudo systemctl set-default graphical.target
+sudo echo -e "[Desktop Entry] \nExec=dwm \nType=XSession" >> /usr/share/xsessions/dwm.desktop
+sudo rm /usr/share/xsessions/lightdm-xsession.desktop
+
 # Configure kitty to use correct theming and transparency
 mkdir -p $HOME/.config/alacritty
 mv $TEMP_DIR/src/alacritty.yml $HOME/.config/alacritty/alacritty.yml
@@ -122,7 +119,6 @@ mv $TEMP_DIR/src/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 mkdir -p $HOME/Pictures
 mv $TEMP_DIR/res/bg.png $HOME/Pictures/bg.png 
 cd $HOME
-feh --bg-scale $HOME/Pictures/bg.png
 
 # Notify user that process terminated successfully
 echo -n "Installation completed successfully. No errors reported."
@@ -162,3 +158,5 @@ while true; do
 		echo "Error: Improper input. Please try again."
 	fi
 done
+startx
+feh --bg-scale $HOME/Pictures/bg.png
