596ce20390a7f9e2b032ee85774234057f8c4407
Aurora <108196668+aur0rae@users.noreply.github.com>
2025-03-19 22:53:29 +0000

    Reverted LightDM implementation

--- install.sh
+++ install.sh
@@ -11,8 +11,7 @@ deb_install() {
 	sudo apt update
 	sudo apt upgrade
 	sudo apt install -y xorg x11-xserver-utils build-essential libx11-dev libxft-dev libxinerama-dev libx11-xcb-dev libxcb-res0-dev libimlib2-dev wget git
-	sudo apt install -y fonts-font-awesome feh policykit-1-gnome nala alacritty flameshot thunar lxappearance pavucontrol neovim flatpak lightdm lightdm-gtk-greeter
-
+	sudo apt install -y fonts-font-awesome feh policykit-1-gnome nala alacritty flameshot thunar lxappearance pavucontrol neovim flatpak 
 	# Configure nala to use best available mirrors
 	sudo nala fetch
 }
@@ -20,8 +19,7 @@ deb_install() {
 arch_install() {
 	sudo pacman -Syu --noconfirm
 	sudo pacman -S --noconfirm base-devel xorg-server xorg-xinit libx11 libxcb cmake libxft libxinerama wget git
-	sudo pacman -S --noconfirm awesome-terminal-fonts feh alacritty picom flameshot thunar polkit-gnome lxappearance pavucontrol neovim flatpak lightdm lightdm-gtk-greeter
-
+	sudo pacman -S --noconfirm awesome-terminal-fonts feh alacritty picom flameshot thunar polkit-gnome lxappearance pavucontrol neovim flatpak
 	# Install paru
 	git clone https://aur.archlinux.org/paru.git
 	cd paru
@@ -84,6 +82,15 @@ cd $TEMP_DIR
 # Edit files to make DWM launch on startup and set background properly
 echo "Configuring DWM to launch on login..."
 
+if [[ ! -f "~/.xinitrc" ]]; then
+	echo "exec dwm" >> ~/.xinitrc
+else
+	echo "Error: ~/.xinitrc already exists. Exiting..."
+	exit
+fi
+
+echo -e 'if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then\n\tstartx\nfi' > ~/.bash_profile
+
 # Configure bash prompt
 echo "Setting custom bash prompt..."
 rm $HOME/.bashrc
@@ -104,12 +111,6 @@ cd /usr/share/themes
 sudo git clone https://github.com/EliverLara/Nordic
 cd $TEMP_DIR
 mv src/gtk-3.0 $HOME/.config/
-
-# Configure LightDM
-sudo systemctl enable lightdm
-sudo systemctl set-default graphical.target
-sudo echo -e "[Desktop Entry] \nExec=dwm \nType=XSession" >> /usr/share/xsessions/dwm.desktop
-sudo rm /usr/share/xsessions/lightdm-xsession.desktop
 
 # Configure kitty to use correct theming and transparency
 mkdir -p $HOME/.config/alacritty
